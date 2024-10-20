4add7922c9dfd1d002d03e5aab395bbfcc2e7226
aarontta-psk <207921825+aarontta-psk@users.noreply.github.com>
2024-10-20 18:00:49 +0100

    WinGLUT

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 
 #include "win_glfw.h"
+#include "win_freeglut.h"
 
 int main(void)
 {
     srand(time(0));
 
-    WinGLFW glfw;
-    glfw.run();
+    //WinGLFW glfw;
+    //glfw.run();
+
+    WinFreeGLUT glut;
 }
---
+++ src/win_freeglut.h
@@ -0,0 +1,19 @@
+#ifndef WIN_GLUT_H
+#define WIN_GLUT_H
+
+class WinFreeGLUT {
+public:
+	WinFreeGLUT();
+	~WinFreeGLUT();
+
+private:
+	int windowID;
+	static float r, g, b;
+
+	//static void close_callback(GLFWwindow* window);
+	static void key_callback(unsigned char key, int x, int y);
+	static void resize_callback(int newWidth, int newHeight);
+	static void display_callback();
+	static void update_callback();
+};
+#endif // WIN_GLFW_H
